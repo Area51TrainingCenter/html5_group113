5c2c07f509493a2fd20f6651205c070db160f89d
Alonso Perez <nuevocorreo17@hotmail.com>
2019-04-03 21:59:09 -0500

    clase18

---
+++ modulo3/clase18/estructuras_repetitivas/js/funciones.js
@@ -0,0 +1,15 @@
+var semana = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+
+/*
+console.log(dias[0]);
+console.log(dias[1]);
+console.log(dias[2]);
+console.log(dias[3]);
+console.log(dias[4]);
+console.log(dias[5]);
+console.log(dias[6]);*/
+
+for (var indice = 0;indice < semana.length ; indice++ ) {
+	console.log(semana[indice]);
+}
+console.log(semana.length);
